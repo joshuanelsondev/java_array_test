1b91d9f53ffb4f94d2379f85b1900737b21857c3
joshuanelsondev <joshuanelsondev@gmail.com>
2025-04-03 21:38:07 -0400

    feat: complete FilterStringsByPrefix

--- src/main/java/org/example/FilterStringsByPrefix.java
+++ src/main/java/org/example/FilterStringsByPrefix.java
@@ -1,15 +1,30 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class FilterStringsByPrefix {
 
     public static String[] filterStringsByPrefix(String[] arr, String prefix) {
-        return new String[] {};
+      if (arr == null || prefix == null)  throw new IllegalArgumentException();
+      ArrayList<String> filteredStringsArr = new ArrayList<>();
+      for (String str : arr) {
+          if (str.contains(prefix)) {
+              filteredStringsArr.add(str);
+          }
+      }
+
+      String[] filteredArr = new String[filteredStringsArr.size()];
+      for (int i = 0; i < filteredStringsArr.size(); i++) {
+          filteredArr[i] = filteredStringsArr.get(i);
+      }
+
+      return filteredArr;
+
     }
 
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-
 
     }
 }
