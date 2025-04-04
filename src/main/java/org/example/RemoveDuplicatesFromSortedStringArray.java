a3a9e50bd4a421036cea84ddb49e7c3589f4e284
joshuanelsondev <joshuanelsondev@gmail.com>
2025-04-03 23:35:35 -0400

    feat: complete RemoveDuplicatesFromSortedStringArray

--- src/main/java/org/example/RemoveDuplicatesFromSortedStringArray.java
+++ src/main/java/org/example/RemoveDuplicatesFromSortedStringArray.java
@@ -1,9 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class RemoveDuplicatesFromSortedStringArray {
 
     public static String[] removeDuplicatesFromSortedStringArray(String[] arr) {
-        return new String[] {};
+        if (arr == null) throw new IllegalArgumentException();
+        ArrayList<String> uniqueStrArr = new ArrayList<>();
+        for (String str : arr) {
+            if (!uniqueStrArr.contains(str)) {
+                uniqueStrArr.add(str);
+            }
+        }
+
+        String[] uniqueStrings = new String[uniqueStrArr.size()];
+        int i = 0;
+        for (String str : uniqueStrArr) {
+           uniqueStrings[i++]  = str;
+        }
+
+       return uniqueStrings;
     }
 
     public static void main( String[] args )
