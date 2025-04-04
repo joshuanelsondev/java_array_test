407da05b19d27ca0a4424f77f2d989965fafd9e2
joshuanelsondev <joshuanelsondev@gmail.com>
2025-04-03 21:09:54 -0400

    feat: complete FilterEvenNumbers

--- src/main/java/org/example/FilterEvenNumbers.java
+++ src/main/java/org/example/FilterEvenNumbers.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class FilterEvenNumbers {
 
     public static int[] filterEvenNumbersFunc(int[] arr) {
-        return new int[] {};
+        if (arr == null) throw new IllegalArgumentException();
+        ArrayList<Integer> evenNums = new ArrayList<>();
+        for (int num : arr) {
+            if (num % 2 == 0) evenNums.add(num);
+        }
+        
+        int[] filteredArr = new int[evenNums.size()];
+        for (int i = 0; i < evenNums.size(); i++) {
+           filteredArr[i]  = evenNums.get(i);
+        }
+        
+        return filteredArr;
     }
 
 
