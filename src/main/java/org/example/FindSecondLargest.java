16b0800371fa2c9119e534136f436991064f236f
joshuanelsondev <joshuanelsondev@gmail.com>
2025-04-03 22:52:12 -0400

    feat: complete FindSecondLargest

--- src/main/java/org/example/FindSecondLargest.java
+++ src/main/java/org/example/FindSecondLargest.java
@@ -3,9 +3,30 @@ package org.example;
 public class FindSecondLargest {
 
     public static int findSecondLargest(int[] arr) {
-        return 0;
+        if (arr == null || arr.length < 2 || areElementsSame(arr)) throw new IllegalArgumentException();
+        int largestNum = arr[0];
+        int secondLargestNum = arr[1];
+        for (int num : arr) {
+            if (num > secondLargestNum && num < largestNum) {
+                secondLargestNum = num;
+            }
+            if (num > largestNum) {
+                secondLargestNum = largestNum;
+                largestNum = num;
+            }
+        }
+        return secondLargestNum;
     }
 
+    public static boolean areElementsSame(int[] arr) {
+        int firstEle = arr[0];
+        for (int num : arr) {
+            if (firstEle != num) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main( String[] args )
     {
